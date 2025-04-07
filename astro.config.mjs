0139a2c8979f17c1de672f4b2815e1f0262e3445
Celia Chavez Virgen <c.chavezvirgen@gmail.com>
2025-04-06 20:57:10 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import { astroImageTools } from "astro-imagetools";
 
 export default defineConfig({
+    site: 'https://webpage-emergency-ground-station-network',
     integrations: [astroImageTools],
 });
