cdd84d9a64370d54ad8aaae73c73cddfff263d29
Celia Chavez Virgen <c.chavezvirgen@gmail.com>
2025-04-11 18:44:56 -0600

    layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-import { astroImageTools } from "astro-imagetools";
 
 export default defineConfig({
-    site: 'https://celiachavezv.github.io/CelestiaCom/',
-    integrations: [astroImageTools],
+  site: 'https://celiachavezv.github.io',
+  base: '/CelestiaCom/',
+  trailingSlash: 'ignore',
+  output: 'static'
 });
+
